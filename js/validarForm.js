f6641103dc4b1f7e8d7f1e2b090c99dc2ca7d2bc
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-05-16 03:15:08 -0300

    Actualizando Lista de funcionários cadastrados 3

--- js/validarForm.js
+++ js/validarForm.js
@@ -1,27 +1,21 @@
-// const nome = document.getElementById('nome')
-// const CPF = document.getElementById('CPF')
-// const RG = document.getElementById('RG')
-// const email = document.getElementById('email')
-// const Nacionalidade = document.getElementById('Nacionalidade')
-// const nascimento = document.getElementById('nascimento')
-// const admissão = document.getElementById('admissão')
-// const remuneração = document.getElementById('remuneração')
-// const horaTrabalho = document.getElementById('horaTrabalho')
-// const Cargo = document.getElementById('Cargo')
-// const numero = document.getElementById('numero')
+const nome = document.getElementById('nome')
+const CPF = document.getElementById('CPF')
+const RG = document.getElementById('RG')
+const email = document.getElementById('email')
+const Nacionalidade = document.getElementById('Nacionalidade')
+const nascimento = document.getElementById('nascimento')
+const admissão = document.getElementById('admissão')
+const remuneração = document.getElementById('remuneração')
+const horaTrabalho = document.getElementById('horaTrabalho')
+const Cargo = document.getElementById('Cargo')
+const numero = document.getElementById('numero')
+const masculino = document.getElementById('masculino')
+const feminina = document.getElementById('feminina')
+const Estatud = document.getElementById('Estatud')
+const contratacao = document.getElementById('contratacao')
+const departSelection = document.getElementById('departSelection')
+const outro = document.getElementById('outro')
 
 
-// function validar(item){
-//     item.setCusttomValidity(' ');
-//     item.checkValidity();
-// }
-// if(item ==)
 
-function valida() {
-    if (document.cadastro3.cpf.validity.patternMismatch) {
-       alert("O CPF está incorreto");
-    } else {
-       alert("O CPF está correto");
-    }
-    return false;
- }
+
