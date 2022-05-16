ad47f00f12c4ab0ce535fb5f369cacaaf5d7e55c
Jhon Manuel <jhonmanuelg6@outlook.com>
2022-05-16 11:51:02 -0300

    Creando validação dos campo

--- js/validarForm.js
+++ js/validarForm.js
@@ -1,21 +1,42 @@
-const nome = document.getElementById('nome')
-const CPF = document.getElementById('CPF')
-const RG = document.getElementById('RG')
-const email = document.getElementById('email')
-const Nacionalidade = document.getElementById('Nacionalidade')
-const nascimento = document.getElementById('nascimento')
-const admissão = document.getElementById('admissão')
-const remuneração = document.getElementById('remuneração')
-const horaTrabalho = document.getElementById('horaTrabalho')
-const Cargo = document.getElementById('Cargo')
-const numero = document.getElementById('numero')
-const masculino = document.getElementById('masculino')
-const feminina = document.getElementById('feminina')
-const Estatud = document.getElementById('Estatud')
-const contratacao = document.getElementById('contratacao')
-const departSelection = document.getElementById('departSelection')
-const outro = document.getElementById('outro')
+// const validate = document.getElementById('validate')
+// const nome = document.getElementById('nome')
+// const CPF = document.getElementById('CPF')
+// const RG = document.getElementById('RG')
+// const email = document.getElementById('email')
+// const Nacionalidade = document.getElementById('Nacionalidade')
+// const nascimento = document.getElementById('nascimento')
+// const admissão = document.getElementById('admissão')
+// const remuneração = document.getElementById('remuneração')
+// const horaTrabalho = document.getElementById('horaTrabalho')
+// const Cargo = document.getElementById('Cargo')
+// const numero = document.getElementById('numero')
+// const masculino = document.getElementById('masculino')
+// const feminina = document.getElementById('feminina')
+// const Estatud = document.getElementById('Estatud')
+// const contratacao = document.getElementById('contratacao')
+// const departSelection = document.getElementById('departSelection')
+// const outro = document.getElementById('outro')
 
 
+// function validate (item){
+//    item.setCustomValidity(' ');
+//    item.checkValidity();
+// if (item == salvar){
+//         if(item.value === salvar.value) item.setCustomValidity('');
+//         else item.setCustomValidity('o campo esta incorrecto. ')
+//     }
+//     if (item == nascimento){
+//        let hoje = new Date();
+//        let dnascimento = new Date(nascimento.value);
 
+//        let idade = hoje.getFullYear() - departSelection.getFullYear();
+//        let m = hoje.getMonth() - departSelection.getMonth();
+//        if(m < 0 || (m === 0 && hoje.getDate() < departSelection())){
+//            idade--;
+//        }
+//        if(idade >= 0) document.getElementById('nascimento').value = idade + ' anos ';
+//        else document.getElementById('nascimento').value = "Indique sua data"
+//     }
+
+// }
 
